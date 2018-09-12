be4fa13b61ed9dece2b79957038650f157880430
MarioMujicaBarcenas <43052713+MarioMujicaBarcenas@users.noreply.github.com>
2018-09-12 17:46:01 -0500

    archivos correctos
    modificacion de archivos correctos

--- AppData/Config/Autoload.php
+++ AppData/Config/Autoload.php
@@ -1,20 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: User
- * Date: 9/12/2018
- * Time: 3:39 PM
- */
 namespace AppData\Config;
-
-class
+class Autoload
+{
+    public static function run()
     {
-         public static function run()
-            {
-                spl_autoload_register(function($class)
-                {
-                    $ruta=str_replace("\\",'/',$class).'.php';
-                    require_once ($ruta);
-                });
-            }
+        spl_autoload_register(function ($class)
+        {
+            $ruta=str_replace("\\",'/',$class).'.php';
+            require_once ($ruta);
+        });
     }
+}
--- AppData/Config/Enrutador.php
+++ AppData/Config/Enrutador.php
@@ -1,51 +1,37 @@
-<?php 
+<?php
 namespace AppData\Config;
-
 class Enrutador
-	{
-		public static function run(Request $request)
-		{
-			$controlador = $request->getControlador(). "Controler";
-			$ruta = ROOT . "AppData" . DS . "Controler" . DS . $controlador .".php";
-			$metodo = $request->getArgumento();
-			$argumento = $request->getArgumento();
-
-
-			if (is_readable($ruta)) 
-			{
-				require_once($ruta);
-				$mostrar = "AppData\\Controler\\" . $controler;
-				$controlador = new $master;
-				if (!isset($argumento)) 
-				{
-					$datos = call_user_func(array($controlador, $metodo));
-				}
-				else
-				{
-					$datos = call_user_func(array($controlador,$metodo),$argumento);
-				}
-				if ($request->getMetodo() !="modificar") 
-				{
-					$ruta = ROOT . "Views" . DS . $request->getControlador() . DS . $request->getMetodo() . ".php";
-					if (is_readable($ruta)) 
-					{
-						require_once($ruta);
-					}
-					else
-						if ($request->getMetodo() == "eliminar" || $request->getMetodo() == "actualizar" || $request->getMetodo() == "crear") 
-						{
-							$ruta = ROOT . "Views" . DS . $request->getControlador() . DS . "tabla" . ".php";
-							if (is_readable($ruta)) 
-							{
-								require_once($ruta);
-							}
-							else
-								echo "Esta pagina no existe";
-						}
-				}
-				else
-					echo(json_encode(mysql_fetch_assoc($datos));
-			}
-		}
-	}
- ?>}
+{
+    public static function run(Request $request)
+    {
+        $controlador = $request->getControlador() . "Controller";
+        $ruta = ROOT . "AppData" . DS . "Controller" . DS . $controlador . ".php";
+        $metodo = $request->getMetodo();
+        $argumento = $request->getArgumento();
+        if (is_readable($ruta)) {
+            require_once($ruta);
+            $mostrar = "AppData\\Controller\\" . $controlador;
+            $controlador = new $mostrar;
+            if (!isset($argumento)) {
+                $datos = call_user_func(array($controlador, $metodo));
+            } else {
+                $datos = call_user_func(array($controlador, $metodo), $argumento);
+            }
+        }
+        if ($request->getMetodo() != "modificar") {
+            $ruta = ROOT . "Views" . DS . $request->getControlador() . DS . $request->getMetodo() . ".php";
+            if (is_readable($ruta))
+                require_once($ruta);
+            else
+                if ($request->getMetodo() == "eliminar" || $request->getMetodo() == "actualizar" || $request->getMetodo() == "crear") {
+                    $ruta = ROOT . "Views" . DS . $request->getControlador() . DS . "tabla" . ".php";
+                    if (is_readable($ruta))
+                        require_once($ruta);
+                    else
+                        echo "Esta página no existe";
+                }
+        } else {
+            echo(json_encode(mysqli_fetch_assoc($datos)));
+        }
+    }
+}
