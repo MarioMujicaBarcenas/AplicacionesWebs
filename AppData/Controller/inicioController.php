a3d1a2d2d94d3e699fa6dc0cfbeb3b0031ce1718
MarioMujicaBarcenas <43052713+MarioMujicaBarcenas@users.noreply.github.com>
2018-09-13 08:21:37 -0500

    actualizacion
    actualizacion

--- AppData/Controller/inicioController.php
+++ AppData/Controller/inicioController.php
@@ -4,30 +4,7 @@ class inicioController
 {
     public function index()
     {
+      
     }
-    public function Habitaciones()
-    {
 
-    }
-    public function Asignar_Productos()
-    {
-    }
-    public function Check_in()
-    {
-    }
-    public function Check_out()
-    {
-    }
-    public function Asignar_Servicios()
-    {
-    }
-    public function Lavanderia()
-    {
-    }
-    public function Servicios()
-    {
-    }
-    public function Admin_Almacen()
-    {
-    }
 }
