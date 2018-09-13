54428afa181efde507278bd60df5ef0f8a78452b
SEBAS8 <alucard_sebas@hotmail.com>
2018-09-13 08:06:49 -0500

    no se

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
+<?php header("content-Type:text/html;charset=utf-8");?>
+<<?php
+  define('DS',DIRECTORY_SEPARATOR);
+  define('ROOT',realpath(dirname(__FILE__)).DS);
+  define('URL',"http://localhost/aplicacionesweb");
+  require_once("AppData/config/Autoload.php");
+  \AppData\config\Autoload::run();
+  if (!isset($_SERVER["HTPP_X_REQUEST_WITH"])&&(isset($_GET['url'])?!((stristr($_GET['url'],'print'))&&(stristr($_GET['url'],'pdf'))):true))
+   {
+     Views\Template::header();
+     AppData\config\Enrutador::run(new AppData\config\Request());
+     views\Template::nav();
+     views\Template::main();
+     views\Template::footer();
+         # code...
+  }
+
+ ?>
 /**
  * Created by PhpStorm.
  * User: User
