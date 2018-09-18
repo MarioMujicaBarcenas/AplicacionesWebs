f2110eaf0fe2d2d024996dd13e94b64bac4e0f59
MarioMujicaBarcenas <43052713+MarioMujicaBarcenas@users.noreply.github.com>
2018-09-18 17:04:29 -0500

    prueba
    prueba

--- Views/template/main.php
+++ Views/template/main.php
@@ -1,6 +1,17 @@
 <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
       <div class="col-md-5 p-lg-5 mx-auto my-5">
-
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        
       </div>
       <div class="product-device shadow-sm d-none d-md-block"></div>
       <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
