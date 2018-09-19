ab33cb5c071466cb39adcc45fede7493962f787b
MarioMujicaBarcenas <43052713+MarioMujicaBarcenas@users.noreply.github.com>
2018-09-19 18:49:43 -0500

    Cambios de Menu

--- Views/template/nav.php
+++ Views/template/nav.php
@@ -1,39 +1,34 @@
 <nav class="navbar fixed-top bg-dark">
-    <ul class="nav nav-tabs" style="margin-left:10em;">
+    <ul class="nav nav-tabs" style="margin-left:12em;">
       <li >
         <button class="btn btn-outline-light  my-2 my-sm-0" type="submit">TESVB</button>
       </li>
       <li class="nav-item dropdown" style="margin-left:1em;">
-      <button class="nav-link dropdown-toggle btn-outline-light " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Jefe de Division</button>
+      <button class="nav-link dropdown-toggle btn-outline-light " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Calificaciones</button>
       <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Jefe</a>
-      <a class="dropdown-item" href="#">de</a>
-      <a class="dropdown-item" href="#">Division</a>
-      </li>
-      <li class="nav-item dropdown" style="margin-left:1em;">
-      <button class="nav-link dropdown-toggle btn-outline-light " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profesores</button>
-      <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Agregar</a>
-      <a class="dropdown-item" href="#">Calificaciones</a>
+      <a class="dropdown-item" href="#">Ver</a>
+      <a class="dropdown-item" href="#">Acentar</a>
       <a class="dropdown-item" href="#">Modificar</a>
       </li>
       <li class="nav-item dropdown" style="margin-left:1em;">
-      <button class="nav-link dropdown-toggle btn-outline-light " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Alumnos</button>
+      <button class="nav-link dropdown-toggle btn-outline-light " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Materias</button>
       <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Materias</a>
-      <a class="dropdown-item" href="#">Calificaciones</a>
-      <a class="dropdown-item" href="#">Comentarios</a>
+      <a class="dropdown-item" href="#">Asignar</a>
       </li>
       <li class="nav-item dropdown" style="margin-left:1em;">
-      <button class="nav-link dropdown-toggle btn-outline-light " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Calificaciones</button>
+      <button class="nav-link dropdown-toggle btn-outline-light " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Reportes</button>
       <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Buscar</a>
-      <a class="dropdown-item" href="#">Calificacion</a>
-      <a class="dropdown-item" href="#">Carreras</a>
+      <a class="dropdown-item" href="#">Materia</a>
+      <a class="dropdown-item" href="#">Grupo</a>
+      <a class="dropdown-item" href="#">Aprobacion</a>
       </li>
       <form class="form-inline mt-2 mt-md-0" style="margin-left:1em;">
             <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
             <button class="btn btn-outline-light  my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
+      <form class="form-inline mt-2 mt-md-0" style="margin-left:1em;">
+            <button class="btn btn-outline-light  my-2 my-sm-0" type="submit" href="<?php  ?>">Login</button>
+            <button class="btn btn-outline-light  my-2 my-sm-0" style="margin-left:1em; type="submit" href="<?php  ?>">Registrar</button>
       </form>
     </ul>
 </nav>
