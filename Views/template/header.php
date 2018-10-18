7bc2489a1d9ea072fb377ba389326fdbff008734
MarioMujicaBarcenas <43052713+MarioMujicaBarcenas@users.noreply.github.com>
2018-10-17 22:46:24 -0500

    Modificacion en Header
    Lineas de Validacion y SweetAlert

--- Views/template/header.php
+++ Views/template/header.php
@@ -3,11 +3,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
     <title>Calificaciones TESVB</title>
-    <!-- Bootstrap core CSS -->
-    <link href="Public/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo URL ?>Public/css/sweetalert2.min.css" rel="stylesheet">
-    <script type="text/javascript" src="Public/js/jquery.min.js"></script>
-    <script type="text/javascript" src="Public/js/popper.min.js"></script>
-    <script type="text/javascript" src="Public/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/Registro.css">
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/login.css">
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/validate.css">
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/bootstrap.css.map">
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/bootstrap.min.css.map">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script type="text/javascript" src="<?php echo URL ?>Public/js/bootstrap.js"></script>
+    <script type="text/javascript" src="<?php echo URL ?>Public/js/jquery.js"></script>
+    <script type="text/javascript" src="<?php echo URL ?>Public/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo URL ?>Public/js/jquery.validate.min.js"></script>
+    <link rel="stylesheet" href="<?php echo URL ?>Public/css/sweetalert.css">
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.min.css'>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.12.15/dist/sweetalert2.all.min.js"></script>
+
   </head>
